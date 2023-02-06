ba977a2397091b894d76ca4b21215c306dd40414
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2023-02-06 23:46:20 +0530

    completed

--- Main.sol
+++ Main.sol
@@ -1,3 +1,41 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
+contract lottery{
+    address public manager;
+    address payable[] public participants;
+
+//In the starting of contract we inform that manager has the control of contract by giving the address of contract.
+    constructor()  
+    {
+        manager = msg.sender;  //msg.sender will get the address of contract 
+    }
+
+    receive() external payable //this is special inbuilt func. used to get ether from participants in contract mostly used with external keyword and it is always payable and it has no parameters
+    {
+         //this is like if statemenet below code is executed if require statement is true.
+        require(msg.value == 2 ether);
+        participants.push(payable(msg.sender));
+    }
+    function getbalance() public view returns(uint) {
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+    //https://ethereum.stackexchange.com/questions/11572/how-does-the-keccak256-hash-function-work/45585,
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, participants.length))); //this hash function is used to generate random from participants it is converted to uint. It is not usually used
+    }
+    //we use the logic that we take modulo of random generated value with lenth of participants since on taking the moduo the number generated will lie from 0-lengthOfParticipants.
+    function selectWinner() public {
+        require(msg.sender == manager);
+        require(participants.length >= 3);
+
+        uint rand = random();
+        uint len = participants.length;
+        address payable winner;
+        uint index = rand%len;
+        winner = participants[index];
+        winner.transfer(getbalance());
+        participants = new address payable[](0); //reseting the participants array
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Introducing my latest project, a Lottery Application built using the Solidity pr
 The basic algorithm of the application allows users to participate in a lottery by submitting their own unique numbers. The winner is then chosen at random and is awarded the prize. The code is thoroughly commented, making it easy to understand for anyone who wants to learn from it or build upon it.
 
 Take a look at the code and see for yourself the power of Solidity in building decentralized applications. Feedback and suggestions for improvements are always welcome
+
+# Algo of project
+1.participant must have wallet.
+2.A participant can transfer ether more than one time but the transferred ether must be 2ether.
+3.As the participant will transfer ether its address will be registered. 
+4.Mnager will have full control over the lottery.
+5.The contract will be reset once a round is completed.
